8f282460713ae430d4e5980b8fc8a0f24dfe1085
dgzct11 <60042905+dgzct11@users.noreply.github.com>
2023-04-23 23:26:50 -0500

    Update ozone.py

--- experiments_with_ltcs/ozone.py
+++ experiments_with_ltcs/ozone.py
@@ -16,22 +16,23 @@ from threading import Thread
 
 
 def log_utils(model, size, dataset, t):
+    csvfile =  open(f"./runs/utils_logger_{dataset}_{model}_{size}_{t}.csv", 'w')
+    csvfile.write("hello world")
     with jtop() as jetson:
         # Make csv file and setup csv
-        with open(f"./runs/utils_logger_{dataset}_{model}_{size}_{t}.csv", 'w') as csvfile:
+        stats = jetson.stats
+        # Initialize cws writer
+        writer = csv.DictWriter(csvfile, fieldnames=stats.keys())
+        # Write header
+        writer.writeheader()
+        # Write first row
+        writer.writerow(stats)
+        # Start loop
+        while jetson.ok():
             stats = jetson.stats
-            # Initialize cws writer
-            writer = csv.DictWriter(csvfile, fieldnames=stats.keys())
-            # Write header
-            writer.writeheader()
-            # Write first row
+            # Write row
             writer.writerow(stats)
-            # Start loop
-            while jetson.ok():
-                stats = jetson.stats
-                # Write row
-                writer.writerow(stats)
-                print(stats)
+            print(stats)
                 
 
 
